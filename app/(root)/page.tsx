4f2ab85b213f4b97aeeb19836a0bad1e0b7e7042
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-05 20:57:36 -0400

    moved to (root)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div>
+      <Button variant={"secondary"} size={"lg"}>
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default page;
