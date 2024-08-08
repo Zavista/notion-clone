caab50b8a2d9bad5867db0413a3ef64156bf8d34
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-07 22:55:37 -0400

    imported Header component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,11 @@
+import Heading from "./_components/Heading";
+
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10"></div>
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading></Heading>
+      </div>
     </div>
   );
 };
