d1933eb9c6a52f13a6029c561d83ecc41ab3fefa
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-07 23:02:23 -0400

    import Hero component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "./_components/Heading";
+import Hero from "./_components/Hero";
 
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading></Heading>
+        <Hero></Hero>
       </div>
     </div>
   );
