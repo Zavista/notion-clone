a4ee006024dfe957b1ce1574182e7d675b647191
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-07 22:50:44 -0400

    Marketing Page setup

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-
-const page = () => {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button variant={"secondary"} size={"lg"}>
-        Click Me Hi
-      </Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10"></div>
     </div>
   );
 };
 
-export default page;
+export default MarketingPage;
