021498ad56ae27cf77dce680d126ad3cdfe6c0a5
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-08 22:53:34 -0400

    Add title and images

--- app/(marketing)/_components/Hero.tsx
+++ app/(marketing)/_components/Hero.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h=[350x] md:w-[400px] md:h-[400px]">
+          <Image
+            src="/documents.png"
+            fill
+            className="object-contain"
+            alt="Documents"
+          ></Image>
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="Reading"
+          ></Image>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
