abc22a710695073924141e16c9971b4a8144bf40
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 17:34:14 -0400

    added create document logic

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,28 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
+
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const DocumentPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = async () => {
+    const toastId = toast.loading("Creating a new note...");
+
+    try {
+      await create({ title: "Untitled" });
+
+      toast.dismiss(toastId);
+      toast.success("New Note created!");
+    } catch (error) {
+      toast.dismiss(toastId);
+      toast.error("Failed to create a new note.");
+    }
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -27,7 +46,7 @@ const DocumentPage = () => {
       <h2 className="text-large font-medium">
         Welcome to {user?.username}&apos;s Jotion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2"></PlusCircle>
         Create a Note
       </Button>
