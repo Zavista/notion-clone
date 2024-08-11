da169831d8ddcee5b3713ba6cb1d5897b242f7f0
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 12:16:11 -0400

    ConvexProvider created

---
+++ components/providers/ConvexProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+type ConvexClientProviderProps = {
+  children: ReactNode;
+};
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
