ec2114ea37c8734d967ca2a449157d502cca143c
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 16:15:22 -0400

    added image to DocumentPage

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentPage = () => {
-  return <div>this is a protected DocumentPage</div>;
+  const { user } = useUser();
+
+  console.log(user);
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      ></Image>
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      ></Image>
+      <h2 className="text-large font-medium">
+        Welcome to {user?.username}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2"></PlusCircle>
+        Create a Note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentPage;
