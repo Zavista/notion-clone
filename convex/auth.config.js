6abfff20a892669b6c1820c51ecaca71cb7ec3d2
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 13:12:46 -0400

    Fixed bug where user logged in but not shown in Nav

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://unique-seasnail-75.clerk.accounts.devclear",
+      domain: "https://unique-seasnail-75.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
