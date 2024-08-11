c61655a2531f70f474b4b0027df1aac4b4d5e996
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 13:22:56 -0400

    Added logic for what to display if user is authenticated

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/Spinner";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +20,26 @@ const Heading = () => {
         Jotion is the connected workspace where <br></br>
         better, faster work happens
       </h3>
-      <Button>
-        Enter Jotion <ArrowRight className="h-4 w-4 ml-2"></ArrowRight>
-      </Button>
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            {" "}
+            Enter Jotion <ArrowRight className="h-4 w-4 ml-2"></ArrowRight>
+          </Link>
+        </Button>
+      )}
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg"></Spinner>{" "}
+        </div>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Jotion Free <ArrowRight className="h-4 w-4 ml-2"></ArrowRight>
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
