d144e334530995e6cc8d98ed5e56460bc7d1d4c6
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 18:28:49 -0400

    Added logic to make item reusable

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,23 +1,71 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
+import { ChevronDown, LucideIcon, ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-type ItemsProps = {
-  onClick: () => void;
+type ItemProps = {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
   label: string;
+  onClick?: () => void;
   icon: LucideIcon;
 };
 
-const Item = ({ label, onClick, icon: Icon }: ItemsProps) => {
+const Item = ({
+  id,
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group  min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className={cn(
+        `group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5
+        flex items-center text-muted-foreground font-medium`,
+        active && "bg-primary/5 text-primary"
+      )}
     >
-      <Icon className="shrink-0 h-[18px] mr-2"></Icon>
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-netrual-600 mr-1"
+          onClick={() => {}}
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted/50"></ChevronIcon>
+        </div>
+      )}
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2"></Icon>
+      )}
       {label}
+
+      {isSearch && (
+        <kbd
+          className="ml-auto pointer-events-none inline-flex gap-1 items-center h-5 select-none rounded border
+          bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100"
+        >
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      )}
     </div>
   );
 };
