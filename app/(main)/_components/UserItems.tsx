2a6394ded6759f7d072b40db7d665037e6e2b91b
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-11 17:08:04 -0400

    Created UserItems

--- app/(main)/_components/UserItems.tsx
+++ app/(main)/_components/UserItems.tsx
@@ -1,7 +1,71 @@
 "use client";
 
+import { ChevronsLeftRight } from "lucide-react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SignOutButton, useUser } from "@clerk/clerk-react";
+
 const UserItems = () => {
-  return <div>UserItems</div>;
+  const { user } = useUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+        >
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl}></AvatarImage>
+            </Avatar>
+            <span className="text-start font-medium line-clamp-1">
+              {user?.username}&apos;s Jotion
+            </span>
+          </div>
+          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4"></ChevronsLeftRight>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-80"
+        align="start"
+        alignOffset={11}
+        forceMount
+      >
+        <div className="flex flex-col space-y-2 p-2">
+          <p className="text-xs font-medium leading-none text-muted-foreground">
+            {user?.emailAddresses[0].emailAddress}
+          </p>
+          <div className="flex items-center gap-x-2">
+            <div className="rounded-md bg-secondary p-1">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={user?.imageUrl}></AvatarImage>
+              </Avatar>
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm line-clamp-1">
+                {user?.username}&apos;s Jotion
+              </p>
+            </div>
+          </div>
+        </div>
+        <DropdownMenuSeparator></DropdownMenuSeparator>
+        <DropdownMenuItem
+          className="w-full cursor-pointer text-muted-foreground"
+          asChild
+        >
+          <SignOutButton>Log Out</SignOutButton>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserItems;
